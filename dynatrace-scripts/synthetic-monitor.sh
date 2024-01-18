f3ea6152b3d78d7a336a76de6a2de812a640af26
root <root@ip-172-31-27-9.us-east-2.compute.internal>
2024-01-19 00:33:58 +1100

    Push synthetic payload

--- dynatrace-scripts/synthetic-monitor.sh
+++ dynatrace-scripts/synthetic-monitor.sh
@@ -3,7 +3,7 @@
 PAYLOAD=$(cat <<EOF
 {
   "name": "$1 $2-$3",
-  "frequencyMin": 0,
+  "frequencyMin": 5,
   "enabled": true,
   "type": "BROWSER",
   "createdFrom": "GUI",
@@ -22,327 +22,256 @@ PAYLOAD=$(cat <<EOF
     "events": [
       {
         "type": "navigate",
-        "wait": {
-          "waitFor": "page_complete"
-        },
         "description": "Loading of \"http://${PUBLIC_IP}:$4\"",
         "url": "http://${PUBLIC_IP}:$4"
+        "wait": {
+          "waitFor": "page_complete"
+        }
       },
       {
         "type": "click",
-        "description": "click on \"Login\"",
+        "description": "click",
         "target": {
           "locators": [
             {
               "type": "css",
-              "value": "a:contains(\"Login\")"
+              "value": "button[type=\"submit\"]:eq(1)"
             },
             {
               "type": "css",
-              "value": ".btn:eq(0)"
+              "value": "button:contains(\"Log in as\")"
             },
             {
               "type": "css",
-              "value": "html body:nth-child(8) div:nth-child(2) div p:nth-child(5) a"
+              "value": ".MuiButtonBase-root:eq(4)"
             },
             {
               "type": "css",
-              "value": "body div.container-fluid div.jumbotron p.lead a.btn:eq(0)"
+              "value": "#root div div:nth-child(2) div div div div:nth-child(3) form div:nth-child(2) div button"
+            },
+            {
+              "type": "css",
+              "value": "#root div.MuiStack-root div.MuiBox-root div.MuiPaper-root div.MuiCardContent-root div.MuiStack-root div.MuiGrid2-root form div.MuiGrid2-root div.MuiCardActions-root button.MuiButtonBase-root:eq(1)"
             }
           ]
         },
         "button": 0,
         "wait": {
-          "waitFor": "page_complete"
-        }
-      },
-      {
-        "type": "keystrokes",
-        "description": "keystrokes on \"id_username\"",
-        "target": {
-          "locators": [
-            {
-              "type": "css",
-              "value": "#id_username"
-            },
-            {
-              "type": "css",
-              "value": "input[type=\"text\"][name=\"email\"]"
-            },
-            {
-              "type": "css",
-              "value": "html body:nth-child(8) div:nth-child(2) div div form div:nth-child(2) div:nth-child(2) div input:nth-child(2)"
-            },
-            {
-              "type": "css",
-              "value": "#id_username"
-            }
-          ]
-        },
-        "simulateBlurEvent": true,
-        "textValue": "guestuser1@mybank.com",
-        "masked": false
-      },
-      {
-        "type": "keystrokes",
-        "description": "keystrokes on \"Password:\"",
-        "target": {
-          "locators": [
-            {
-              "type": "css",
-              "value": "#password"
-            },
-            {
-              "type": "css",
-              "value": "input[type=\"password\"][name=\"password\"]"
-            },
-            {
-              "type": "css",
-              "value": ".form-control"
-            },
-            {
-              "type": "css",
-              "value": "html body:nth-child(8) div:nth-child(2) div div:nth-child(2) div:nth-child(2) div input:nth-child(2)"
-            },
-            {
-              "type": "css",
-              "value": "#password"
-            }
-          ]
-        },
-        "simulateBlurEvent": true,
-        "textValue": "U2FsdGVkX19M6bloalXign7QvZc/XuQwc3pqyNI5Quk=",
-        "masked": true,
-        "wait": {
-          "waitFor": "page_complete"
+          "waitFor": "network"
         }
       },
       {
         "type": "click",
-        "description": "click on \"Pay Bills\"",
+        "description": "click on \"Instruments\"",
         "target": {
           "locators": [
             {
               "type": "css",
-              "value": "a:contains(\"Pay Bills\")"
+              "value": "a:contains(\"Instruments\")"
             },
             {
               "type": "css",
-              "value": "div:contains(\"Funds Transfer\"):eq(6)"
+              "value": "span:contains(\"Instruments\")"
             },
             {
               "type": "css",
-              "value": ".btn:eq(1)"
+              "value": ".MuiTypography-root:eq(3)"
             },
             {
               "type": "css",
-              "value": "html body:nth-child(8) div:nth-child(3) div div div div:nth-child(2) div:nth-child(3) div a"
+              "value": "#root div div:nth-child(2) div div div:nth-child(2) ul li:nth-child(2) a div:nth-child(2) span"
             },
             {
               "type": "css",
-              "value": "body div.container-fluid div.jumbotron div.container div.row div.col-md-12 div.wrapper div.centered a.btn:eq(0)"
+              "value": "#root div.MuiStack-root div.MuiBox-root div.MuiDrawer-root div.MuiPaper-root div.MuiBox-root ul.MuiList-root li.MuiListItem-root a.MuiButtonBase-root div.MuiListItemText-root span.MuiTypography-root:eq(1)"
+            }
+          ]
+        },
+        "button": 0
+      },
+      {
+        "type": "click",
+        "description": "click on \"Credit Card\"",
+        "target": {
+          "locators": [
+            {
+              "type": "css",
+              "value": "a:contains(\"Credit Card\")"
+            },
+            {
+              "type": "css",
+              "value": "span:contains(\"Credit Card\")"
+            },
+            {
+              "type": "css",
+              "value": ".MuiTypography-root:eq(6)"
+            },
+            {
+              "type": "css",
+              "value": "#root div div:nth-child(2) div div div:nth-child(2) ul li:nth-child(5) a div:nth-child(2) span"
+            },
+            {
+              "type": "css",
+              "value": "#root div.MuiStack-root div.MuiBox-root div.MuiDrawer-root div.MuiPaper-root div.MuiBox-root ul.MuiList-root li.MuiListItem-root a.MuiButtonBase-root div.MuiListItemText-root span.MuiTypography-root:eq(4)"
             }
           ]
         },
         "button": 0,
         "wait": {
-          "waitFor": "page_complete"
+          "waitFor": "network"
         }
       },
       {
-        "type": "keystrokes",
-        "description": "keystrokes on \"Amount\"",
+        "type": "click",
+        "description": "click on \"Autofill\"",
         "target": {
           "locators": [
             {
               "type": "css",
-              "value": "#amount"
+              "value": "button[type=\"button\"]:eq(4)"
             },
             {
               "type": "css",
-              "value": "input[type=\"text\"][name=\"amount\"]"
+              "value": "button:contains(\"Autofill\")"
             },
             {
               "type": "css",
-              "value": ".form-control"
+              "value": ".MuiButtonBase-root:eq(12)"
             },
             {
               "type": "css",
-              "value": "html body:nth-child(12) div:nth-child(3) form div div div:nth-child(2) div:nth-child(2) div div:nth-child(2) input:nth-child(2)"
+              "value": "#root div div:nth-child(2) div:nth-child(2) div div div div div:nth-child(2) form div:nth-child(7) div button:nth-child(2)"
             },
             {
               "type": "css",
-              "value": "#amount"
+              "value": "#root div.MuiStack-root div.MuiBox-root div.MuiGrid2-root div.MuiBox-root div.MuiPaper-root div.MuiCardContent-root div.MuiStack-root div.MuiGrid2-root form div.MuiGrid2-root div.MuiCardActions-root button.MuiButtonBase-root:eq(1)"
             }
           ]
         },
-        "simulateBlurEvent": true,
-        "textValue": "10",
-        "masked": false
+        "button": 0
       },
       {
         "type": "click",
-        "description": "click on \"WITHDRAW\"",
+        "description": "click on \"submitButton\"",
         "target": {
           "locators": [
+            {
+              "type": "css",
+              "value": "#submitButton"
+            },
             {
               "type": "css",
               "value": "button[type=\"submit\"]"
             },
             {
               "type": "css",
-              "value": "button:contains(\"WITHDRAW\")"
+              "value": "button:contains(\"Order card\")"
             },
             {
               "type": "css",
-              "value": ".btn:eq(1)"
+              "value": ".MuiButtonBase-root:eq(11)"
             },
             {
               "type": "css",
-              "value": "html body:nth-child(12) div:nth-child(3) form div div div:nth-child(3) div:nth-child(2) button"
+              "value": "#root div div:nth-child(2) div:nth-child(2) div div div div div:nth-child(2) form div:nth-child(7) div button"
             },
             {
               "type": "css",
-              "value": "body div.container-fluid form div.jumbotron div.container div.row div.col-md-6 button.btn"
+              "value": "#submitButton"
             }
           ]
         },
         "button": 0,
         "wait": {
-          "waitFor": "page_complete"
+          "waitFor": "network"
         }
       },
       {
         "type": "click",
-        "description": "click on \"Funds Transfer\"",
+        "description": "click on \"Home\"",
         "target": {
           "locators": [
             {
               "type": "css",
-              "value": "a:contains(\"Funds Transfer\")"
+              "value": "a:contains(\"Home\")"
             },
             {
               "type": "css",
-              "value": "div:contains(\"Funds Transfer\"):eq(6)"
+              "value": "span:contains(\"Home\")"
             },
             {
               "type": "css",
-              "value": ".btn:eq(2)"
+              "value": ".MuiTypography-root:eq(2)"
             },
             {
               "type": "css",
-              "value": "html body:nth-child(8) div:nth-child(3) div div div div:nth-child(2) div:nth-child(3) div a:nth-child(3)"
+              "value": "#root div div:nth-child(2) div div div:nth-child(2) ul li a div:nth-child(2) span"
             },
             {
               "type": "css",
-              "value": "body div.container-fluid div.jumbotron div.container div.row div.col-md-12 div.wrapper div.centered a.btn:eq(1)"
+              "value": "#root div.MuiStack-root div.MuiBox-root div.MuiDrawer-root div.MuiPaper-root div.MuiBox-root ul.MuiList-root li.MuiListItem-root a.MuiButtonBase-root div.MuiListItemText-root span.MuiTypography-root:eq(0)"
             }
           ]
         },
-        "button": 0,
-        "wait": {
-          "waitFor": "page_complete"
-        }
-      },
-      {
-        "type": "keystrokes",
-        "description": "keystrokes on \"Amount\"",
-        "target": {
-          "locators": [
-            {
-              "type": "css",
-              "value": "#amount"
-            },
-            {
-              "type": "css",
-              "value": "input[type=\"text\"][name=\"amount\"]"
-            },
-            {
-              "type": "css",
-              "value": ".form-control"
-            },
-            {
-              "type": "css",
-              "value": "html body:nth-child(12) div:nth-child(3) form div div div:nth-child(2) div:nth-child(2) div div:nth-child(2) input:nth-child(2)"
-            },
-            {
-              "type": "css",
-              "value": "#amount"
-            }
-          ]
-        },
-        "simulateBlurEvent": true,
-        "textValue": "2",
-        "masked": false
+        "button": 0
       },
       {
         "type": "click",
-        "description": "click on \"DEPOSIT\"",
+        "description": "click on \"J\"",
         "target": {
           "locators": [
             {
               "type": "css",
-              "value": "button[type=\"submit\"]"
+              "value": "div:contains(\"J\"):eq(5)"
             },
             {
               "type": "css",
-              "value": "button:contains(\"DEPOSIT\")"
+              "value": ".MuiAvatar-root"
             },
             {
               "type": "css",
-              "value": ".btn:eq(1)"
+              "value": "#profileToggler div"
             },
             {
               "type": "css",
-              "value": "html body:nth-child(12) div:nth-child(3) form div div div:nth-child(3) div:nth-child(2) button"
-            },
-            {
-              "type": "css",
-              "value": "body div.container-fluid form div.jumbotron div.container div.row div.col-md-6 button.btn"
+              "value": "#profileToggler div.MuiAvatar-root"
             }
           ]
         },
-        "button": 0,
-        "wait": {
-          "waitFor": "page_complete"
-        }
+        "button": 0
       },
       {
         "type": "click",
-        "description": "click on \"Logout\"",
+        "description": "click on \"logoutItem\"",
         "target": {
           "locators": [
             {
               "type": "css",
-              "value": "a:contains(\"Logout\")"
+              "value": "#logoutItem"
             },
             {
               "type": "css",
-              "value": "div:contains(\"Welcome User-1 !!\")"
+              "value": ".MuiButtonBase-root:eq(42)"
             },
             {
               "type": "css",
-              "value": ".btn:eq(0)"
+              "value": "html body:nth-child(2) div:nth-child(4) div:nth-child(3) ul li"
             },
             {
               "type": "css",
-              "value": "html body:nth-child(8) div a:nth-child(2)"
-            },
-            {
-              "type": "css",
-              "value": "body div a.btn:eq(0)"
+              "value": "#logoutItem"
             }
           ]
         },
         "button": 0,
         "wait": {
-          "waitFor": "page_complete"
+          "waitFor": "network"
         }
       }
     ]
   },
   "locations": [
-    "GEOLOCATION-2FD31C834DE4D601"
+    "GEOLOCATION-57F63BAD1C6A415C"
   ],
   "anomalyDetection": {
     "outageHandling": {
@@ -362,17 +291,54 @@ PAYLOAD=$(cat <<EOF
       "thresholds": []
     }
   },
-  "tags": [
-   "$1-$3"
-  ],
+  "tags": [],
   "managementZones": [],
-  "automaticallyAssignedApps": [],
+  "automaticallyAssignedApps": [
+    "APPLICATION-827F4150B32AC679"
+  ],
   "manuallyAssignedApps": [],
   "keyPerformanceMetrics": {
     "loadActionKpm": "VISUALLY_COMPLETE",
     "xhrActionKpm": "VISUALLY_COMPLETE"
   },
-  "events": []
+  "events": [
+    {
+      "name": "Loading of \"http://${PUBLIC_IP}"",
+      "sequenceNumber": 1
+    },
+    {
+      "name": "click",
+      "sequenceNumber": 2
+    },
+    {
+      "name": "click on \"Instruments\"",
+      "sequenceNumber": 3
+    },
+    {
+      "name": "click on \"Credit Card\"",
+      "sequenceNumber": 4
+    },
+    {
+      "name": "click on \"Autofill\"",
+      "sequenceNumber": 5
+    },
+    {
+      "name": "click on \"submitButton\"",
+      "sequenceNumber": 6
+    },
+    {
+      "name": "click on \"Home\"",
+      "sequenceNumber": 7
+    },
+    {
+      "name": "click on \"J\"",
+      "sequenceNumber": 8
+    },
+    {
+      "name": "click on \"logoutItem\"",
+      "sequenceNumber": 9
+    }
+  ]
 }
 EOF
 )
